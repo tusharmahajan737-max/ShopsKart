e96b7b84550f6480f98ef57d1e66bcffc86359cb
Tushar Mahajan <tusharmahajan737@gmail.com>
2026-09-26 09:28:25 +0530

    Update ShopsKart cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,8 @@
+export default function Cart() {
+    return (
+        <section className="container my-5 cart-page">
+            <h1>Your Cart</h1>
+            <p>Your selected items will appear here.</p>
+        </section>
+    )
+}
