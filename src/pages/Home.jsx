78fde89749f7cacaac8134d3b09c279b122742dc
Tushar Mahajan <tusharmahajan737@gmail.com>
2026-09-26 09:26:24 +0530

    Improve ShopsKart home page functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+        <section className="container my-5 home-page">
+            <h1>Welcome to ShopKart Home Page</h1>
+            <p>Discover the latest products and best deals for your shopping needs.</p>
+        </section>
+    )
+}
